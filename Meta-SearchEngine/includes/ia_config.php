b296e3a8ebef76de5754c399d4b79ff63034506e
AlvaroGarSe <132345019+AlvaroGarSe@users.noreply.github.com>
2025-12-29 18:12:53 +0100

    ia config

--- Meta-SearchEngine/includes/ia_config.php
+++ Meta-SearchEngine/includes/ia_config.php
@@ -4,12 +4,8 @@ return [
         "base_url" => "http://localhost/Meta-search-engine-and-e-commerce/IndividualAssignments/IAII_TTRPGShop_UAguillo/api/",
         "api_key"  => "ttrpg-secret-123"
     ],
-    "ttrpg" => [
+    "techShop" => [
         "base_url" => "http://localhost/Meta-search-engine-and-e-commerce/IndividualAssignments/IAII_TechShop_AGarcia/api/",
         "api_key"  => "TECHSHOP_SECRET_123"
     ],
-    "otheria" => [
-        "base_url" => "https://otroia.com/api/",
-        "api_key"  => "other-secret-456"
-    ]
 ];
