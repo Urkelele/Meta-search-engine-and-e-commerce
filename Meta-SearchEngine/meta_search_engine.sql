2a17a8e1967f47a2eb7ba71d5fd9ae40814e8b6e
AlvaroGarSe <132345019+AlvaroGarSe@users.noreply.github.com>
2026-01-06 20:13:22 +0100

    Update SQL dump with new user and order data
    Refreshed the meta_search_engine.sql file to include new user, order, and order item records, and updated AUTO_INCREMENT values accordingly. Removed old test data and added a new user and associated orders. Added GA_PAPI_AlvaroGarcia_UrkoAguillo.zip to the repository.

--- Meta-SearchEngine/meta_search_engine.sql
+++ Meta-SearchEngine/meta_search_engine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-01-2026 a las 21:14:20
+-- Tiempo de generación: 06-01-2026 a las 20:11:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -36,14 +36,6 @@ CREATE TABLE `mse_carts` (
   `quantity` int(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `mse_carts`
---
-
-INSERT INTO `mse_carts` (`id`, `user_id`, `ia_name`, `created_at`, `ia_item_id`, `quantity`) VALUES
-(25, 6, 'ttrpg', '2026-01-02', 1, 1),
-(26, 6, 'ttrpg', '2026-01-02', 6, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -62,13 +54,7 @@ CREATE TABLE `mse_orders` (
 --
 
 INSERT INTO `mse_orders` (`id`, `user_id`, `status`, `created_at`) VALUES
-(1, 1, 'paid', '2025-12-30'),
-(3, 1, 'paid', '2025-12-30'),
-(13, 1, 'paid', '2025-12-30'),
-(14, 1, 'paid', '2026-01-02'),
-(15, 6, 'paid', '2026-01-02'),
-(16, 6, 'paid', '2026-01-02'),
-(19, 9, 'paid', '2026-01-04');
+(22, 15, 'paid', '2026-01-05');
 
 -- --------------------------------------------------------
 
@@ -91,26 +77,10 @@ CREATE TABLE `mse_order_items` (
 --
 
 INSERT INTO `mse_order_items` (`id`, `order_id`, `ia_name`, `ia_item_id`, `quantity`, `price_at_purchase`, `ia_order_ref`) VALUES
-(1, 1, 'ttrpg', 2, 2, 75.00, '8'),
-(2, 1, 'ttrpg', 1, 6, 40.00, '9'),
-(3, 1, 'techShop', 40, 1, 189.00, 'N/A'),
-(6, 3, 'ttrpg', 1, 1, 40.00, '12'),
-(7, 3, 'ttrpg', 4, 1, 49.00, '13'),
-(8, 3, 'techShop', 35, 1, 211.00, 'N/A'),
-(9, 3, 'techShop', 41, 1, 237.00, 'N/A'),
-(28, 13, 'ttrpg', 8, 1, 13.00, '32'),
-(29, 13, 'ttrpg', 9, 1, 33.00, '33'),
-(30, 13, 'techShop', 13, 1, 96.00, 'N/A'),
-(31, 13, 'techShop', 27, 1, 143.00, 'N/A'),
-(32, 14, 'techShop', 33, 1, 192.00, '51'),
-(33, 14, 'techShop', 35, 1, 211.00, '52'),
-(34, 14, 'ttrpg', 4, 1, 49.00, '34'),
-(35, 15, 'ttrpg', 1, 2, 40.00, '35'),
-(36, 15, 'ttrpg', 3, 1, 61.00, '36'),
-(37, 16, 'ttrpg', 9, 1, 33.00, '37'),
-(38, 16, 'techShop', 43, 1, 218.00, '53'),
-(39, 16, 'techShop', 32, 1, 179.00, '54'),
-(44, 19, 'ttrpg', 3, 1, 61.00, '40');
+(53, 22, 'ttrpg', 6, 1, 19.00, '46'),
+(54, 22, 'ttrpg', 7, 1, 35.00, '47'),
+(55, 22, 'techShop', 1, 1, 89.99, '60'),
+(56, 22, 'techShop', 4, 1, 129.99, '61');
 
 -- --------------------------------------------------------
 
@@ -146,13 +116,7 @@ CREATE TABLE `mse_users` (
 --
 
 INSERT INTO `mse_users` (`id`, `email`, `password_hash`, `is_verified`, `created_at`) VALUES
-(1, '123@gmail.com', '$2y$10$b3h/u5xhZLZn7bMNMoYFy.wWIKq97xjN6IrnAMvaOk2/ADjfVSKaG', 1, '2025-12-29'),
-(2, '1234@gmail.com', '$2y$10$3p.nkNNhDo0DsDaZDqBcGetiMxtHDAo4wOb.D.P3THmZ4YF5hW6fa', 1, '2025-12-29'),
-(3, '12345@gmail.com', '$2y$10$BS/0oB1oppUtNOBjNvt2r.VHYX193QG8kUKVz0oVhcUWbsYeoUb6e', 1, '2025-12-29'),
-(4, '1212@gmail.com', '$2y$10$ZaqBBhWNKnaIQMawM.PG3e.d080vqnPR1dvG2dgJIMSsAXRedliPa', 1, '2025-12-29'),
-(6, '121212@gmail.com', '$2y$10$nwPmBiSdIBUBqXAOAcgaZuEK0jeCu2NOfG4MFAC.Y99P4NHq.ljAG', 1, '2026-01-02'),
-(9, 'algarse04@gmail.com', '$2y$10$4SePVA4nyEVxRe7kdXvIUeXI3lqC0puXskndVGEkT0L.lbrh/8eLi', 1, '2026-01-02'),
-(14, 'alu.140009@usj.es', '$2y$10$ufqKAvWsJHBSIBfdtxgvqeru/DwiSVmjwVgW5OmYov2GcUWnhnvJe', 1, '2026-01-04');
+(15, 'algarse04@gmail.com', '$2y$10$MnGU6sUmYV/rMb0UGTilwOpG7L9QRFdVkthAUKJMsz.UhH5FF9R5y', 1, '2026-01-05');
 
 --
 -- Índices para tablas volcadas
@@ -202,31 +166,31 @@ ALTER TABLE `mse_users`
 -- AUTO_INCREMENT de la tabla `mse_carts`
 --
 ALTER TABLE `mse_carts`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de la tabla `mse_orders`
 --
 ALTER TABLE `mse_orders`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `mse_order_items`
 --
 ALTER TABLE `mse_order_items`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT de la tabla `mse_tokens`
 --
 ALTER TABLE `mse_tokens`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `mse_users`
 --
 ALTER TABLE `mse_users`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
